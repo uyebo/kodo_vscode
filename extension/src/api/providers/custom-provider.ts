1041835e9b2e3bce95e96e89b7fd5cbe79e05367
uyebo <qq16349270@gmail.com>
2025-04-09 00:31:50 +0800

    Update custom-provider.ts

--- extension/src/api/providers/custom-provider.ts
+++ extension/src/api/providers/custom-provider.ts
@@ -18,11 +18,7 @@ import { GlobalState, GlobalStateManager } from "../../providers/state/global-st
 import { OpenRouterModelCache } from "./config/openrouter-cache"
 import { getOpenrouterGenerationData } from "./config/openrouter"
 import delay from "delay"
-case PROVIDER_IDS.XIAOAI:
-  if (!settings.apiKey) throw error
-  return createXiaoaiSDK({
-    apiKey: settings.apiKey
-  }).languageModel(modelId)
+
 
 type ExtractCacheTokens = {
 	cacheCreationField: string
@@ -130,6 +126,9 @@ const providerToAISDKModel = (settings: ApiConstructorOptions, modelId: string):
 			return createGoogleGenerativeAI({
 				apiKey: settings.providerSettings.apiKey,
 			}).languageModel(modelId)
+		case PROVIDER_IDS.XIAOAI:
+ 			 if (!settings.apiKey) throw error
+  				return createXiaoaiSDK({ apiKey: settings.apiKey).languageModel(modelId)	
 		case PROVIDER_IDS.OPENAICOMPATIBLE:
 			const providerSettings = customProviderSchema.safeParse(settings.providerSettings)
 			if (!providerSettings.success) {
