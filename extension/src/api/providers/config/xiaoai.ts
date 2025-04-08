c27a207b5c36da493c8970862a456649bdbe27f2
uyebo <qq16349270@gmail.com>
2025-04-09 00:15:18 +0800

    Update xiaoai.ts

--- extension/src/api/providers/config/xiaoai.ts
+++ extension/src/api/providers/config/xiaoai.ts
@@ -1,1 +1,29 @@
 
+// providers/openai.ts
+import { ProviderConfig } from "../types"
+import { DEFAULT_BASE_URLS, PROVIDER_IDS, PROVIDER_NAMES } from "../constants"
+
+export const openaiConfig: ProviderConfig = {
+	id: PROVIDER_IDS.XIAOAI,
+	name: PROVIDER_NAMES[PROVIDER_IDS.XIAOAI],
+	baseUrl: DEFAULT_BASE_URLS[PROVIDER_IDS.XIAOAI],
+	models: [
+		{
+			id: "xiaoai",
+			name: "xiaoai",
+			contextWindow: 200_000,
+			maxTokens: 128000,
+			supportsImages: false,
+			supportsPromptCache: true,
+			inputPrice: 1.1,
+			outputPrice: 4.4,
+			cacheReadsPrice: 1.1 * 0.5, // 50% of input price
+			cacheWritesPrice: 1.1,
+			provider: PROVIDER_IDS.XIAOAI,
+			isThinkingModel: true,
+			reasoningEffort: "high",
+		},
+		
+	],
+	requiredFields: ["apiKey"],
+}
